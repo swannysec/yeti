9c348d5fb4b5cd96fc17da2213de389a8df16cc1
John D. Swanson <swannysec@users.noreply.github.com>
2017-04-04 23:42:07 -0400

    Update celeryctl.py

--- core/config/celeryctl.py
+++ core/config/celeryctl.py
@@ -7,7 +7,7 @@ celery_app = Celery('yeti')
 
 class CeleryConfig:
     # BROKER_URL = 'mongodb://localhost:27017/'
-    BROKER_URL = 'redis://localhost:6379/0'
+    BROKER_URL = 'redis://0.0.0.0:6379/0'
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_ACCEPT_CONTENT = ['json']
     CELERY_IMPORTS = ('core.config.celeryimports', 'core.analytics_tasks', 'core.exports.export', 'core.feed')
